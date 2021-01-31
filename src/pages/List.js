653ea547b69e9f1b36957b1c9e354b473a9e42fe
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2021-01-30 23:52:26 -0300

    Alterações para realizar deploy Heroku

--- src/pages/List.js
+++ src/pages/List.js
@@ -9,7 +9,7 @@ function List() {
   const [musics, setMusics] = useState([]);
 
   useEffect(() => {
-    db.get('/').then(res => {
+    db.get('/musics/').then(res => {
       setMusics(res.data);
     });
   }, []);
