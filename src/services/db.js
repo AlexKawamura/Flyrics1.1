0adec3ee67fd20c78e899e0293ac617ac3259a83
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2021-01-30 23:41:08 -0300

    Alterações para realizar deploy Heroku

--- src/services/db.js
+++ src/services/db.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const db = axios.create({
-  baseURL: 'https://flyrics.herokuapp.com/musics',
+  baseURL: 'https://flyrics.herokuapp.com:4000/musics',
 });
 
 export default db;
